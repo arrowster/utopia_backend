f1d3ebed241104ad6d846a367fbfa5a8e69baeef
arrowster <kjy9835@naver.com>
2024-06-20 16:05:53 +0900

    추가/수정: driver, gmarket_search.py
    -driver의 유지보수를 위한 gmarket_search와의 분리 작업
    -driver은 driver을 초기화하는 initialize_driver와, url 기반으로 soup을 반환하는 get_soup_from_url(driver, url)으로 구성됨
    -gmarket_search는 이에 따라 driver을 이용하여 기존의 작업을 수행 불필요한 부분은 제거됨

--- controllers/gmarket_search.py
+++ controllers/gmarket_search.py
@@ -1,43 +1,13 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from bs4 import BeautifulSoup
+from utopia_backend.controllers.driver import initialize_driver, get_soup_from_url
 
 
 def gmarket_search(words):
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
-    service = Service()
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("window-size=10,10")
-    options.add_argument('user-agent=' + user_agent)
-
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = initialize_driver()
 
     for word in words:
         shop_set = set()
         url = f'https://www.gmarket.co.kr/n/search?keyword={word}&f=o:i&k=0&p=1&v=l'
-        driver.get(url)
-
-        body = driver.find_element(By.CSS_SELECTOR, 'body')
-        body.send_keys(Keys.END)
-        time.sleep(1)
-        try:
-            WebDriverWait(driver, 10).until(
-                expected_conditions.presence_of_all_elements_located((By.TAG_NAME, 'div'))
-            )
-        except Exception as e:
-            print("페이지 로드 중 오류 발생:", e)
-            driver.quit()
-
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        soup = get_soup_from_url(driver, url)
         product_items = soup.find_all('a', class_='link__shop')
         print(product_items)
 
