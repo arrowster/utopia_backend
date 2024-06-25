eb7c97f9500d40d2b52b709fdc8cc4a95968230e
arrowster <kjy9835@naver.com>
2024-06-25 10:16:36 +0900

    추가: pruning_shop_item
    -가지치기를 위한 pruning_shop_item 추가
    -shop_list를 받아서 img_urls를 반환
    -현재 gmarket만 조사함으로 gmarket만 맞춰서 기능 추가

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routers import routers
-from controllers import gmarket_search
+from controllers.gmarket_search import gmarket_search
+from controllers.pruning_shop_item import pruning_shop_item
 
 app = Flask(__name__)
 
@@ -19,9 +20,10 @@ def test_sig_func():
 def market_search_func():
     keywordlist = routers.key_words()
     print(keywordlist)
-    gmarket_search.gmarket_search(keywordlist)
-    #todo: keyword get
-    #todo: naver 마켓 수집
+    shop_list = gmarket_search(keywordlist)
+    print(shop_list)
+    shop_item = pruning_shop_item(shop_list)
+    print(shop_item)
     #todo: keyword 가지치기
     #todo: 배송비 측정
     return keywordlist
---
+++ controllers/pruning_shop_item.py
@@ -0,0 +1,34 @@
+from utopia_backend.controllers.driver import initialize_driver, get_soup_from_url
+
+
+def pruning_shop_item(shop_list):
+    img_urls = []
+    driver = initialize_driver()
+    for shop_url in shop_list:
+        #todo:나중에 쇼핑물 별로 분류 필요
+        img_url = pruning_gmarket_item(shop_url, driver)
+        img_urls.extend(img_url)
+
+    driver.close()
+    driver.quit()
+    return img_urls
+
+
+def pruning_gmarket_item(url, driver):
+    img_urls = []
+    add_url = ('/List?Title=Best%20Item&CategoryType=General&SortType=FocusRank&DisplayType=List&Page=1&PageSize=60'
+               '&IsFreeShipping=False&HasDiscount=False&HasStamp=False&HasMileage=False&IsInternationalShipping=False'
+               '&IsTpl=False&Roles=System.String%5B%5D&NeedToGetSDBrandName=True#listTop')
+
+    shop_url = url + add_url
+    soup = get_soup_from_url(driver, shop_url)
+    all_shop_item = soup.find_all('tr')
+
+    for item in all_shop_item:
+        review_span = item.find('span', {'class': 'cnt'})
+        if review_span:
+            img_tag = item.find('p', {'class': 'img'}).find('img')
+            if img_tag and 'data-original' in img_tag.attrs:
+                print(img_tag['data-original'])
+                img_urls.append(img_tag['data-original'])
+    return img_urls
