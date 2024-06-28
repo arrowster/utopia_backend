37f33d388d2a9ff359f7c2997bced1b268bb316b
arrowster <kjy9835@naver.com>
2024-06-28 13:04:54 +0900

    추가: pruning_gmarket_item 특정 카테고리가 1순위인 마켓 분류 기능 추가
    -공구/안전/산업용품이 카테고리의 최상단일때만 img_url을 수집하도록 수정

--- controllers/pruning_shop_item.py
+++ controllers/pruning_shop_item.py
@@ -5,7 +5,10 @@ def pruning_shop_item(driver, shop_list):
     img_urls = []
     for shop_url in shop_list:
         #todo:나중에 쇼핑물 별로 분류 필요
+
         img_url = pruning_gmarket_item(driver, shop_url)
+        if not img_url:
+            continue
         img_urls.extend(img_url)
 
     return img_urls
@@ -20,12 +23,18 @@ def pruning_gmarket_item(driver, url):
 
     shop_url = url + add_url
     soup = get_soup_from_url(driver, shop_url)
-    all_shop_item = soup.find_all('tr')
 
-    for item in all_shop_item:
-        review_span = item.find('span', {'class': 'cnt'})
-        if review_span:
-            img_tag = item.find('p', {'class': 'img'}).find('img')
-            if img_tag and 'data-original' in img_tag.attrs:
-                img_urls.append(https + img_tag['data-original'])
+    first_li = soup.select_one('#ulCategory > li:nth-child(1)')
+    first_li_name = first_li.find('a').text
+
+    if first_li_name == '공구/안전/산업용품':
+        all_shop_item = soup.find_all('tr')
+        for item in all_shop_item:
+            review_span = item.find('span', {'class': 'cnt'})
+            if review_span:
+                img_tag = item.find('p', {'class': 'img'}).find('img')
+                if img_tag and 'data-original' in img_tag.attrs:
+                    img_urls.append(https + img_tag['data-original'])
+    else:
+        return False
     return img_urls
