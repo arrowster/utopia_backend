dabc84330c806e689dbc1f34380abc03951f495c
arrowster <kjy9835@naver.com>
2024-07-05 16:05:32 +0900

    추가: ShopItem dataclass 요소 추가
    -naver_category: string 네이버 쇼핑 카테고리
     taobao_item_url: string taobao의 상품 url
     main_keywords: list
     sub_keywords: list

--- models/ShopItem.py
+++ models/ShopItem.py
@@ -1,7 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
 class ShopItem:
     item_name: str
     image_url: str
+    naver_category: str = field(default_factory="")
+    taobao_item_url: str = field(default_factory="")
+    main_keywords: List[str] = field(default_factory=list)
+    sub_keywords: List[str] = field(default_factory=list)
