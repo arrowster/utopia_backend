85747423137151ef7e144852306fa249852c3079
arrowster <kjy9835@naver.com>
2024-07-17 19:24:49 +0900

    수정: ShopItem 요소들 이름 수정
    -ShopItem의 요소들의 구성 이름이 모두 달라 앞에 item 접두사를 붙임
    -default_factory=""에서 ""부분을 str으로 명시화 시킴

--- models/ShopItem.py
+++ models/ShopItem.py
@@ -5,8 +5,8 @@ from typing import List
 @dataclass
 class ShopItem:
     item_name: str
-    image_url: str
-    naver_category: str = field(default_factory="")
-    taobao_item_url: str = field(default_factory="")
-    main_keywords: List[str] = field(default_factory=list)
-    sub_keywords: List[str] = field(default_factory=list)
+    item_image_url: str
+    item_naver_category: str = field(default_factory=str)
+    item_taobao_item_url: str = field(default_factory=str)
+    item_main_keywords: List[str] = field(default_factory=list)
+    item_sub_keywords: List[str] = field(default_factory=list)
