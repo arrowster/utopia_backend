90cefd3815f43ab0bd17e551f003216e1d4e52e0
arrowster <kjy9835@naver.com>
2024-07-29 16:01:16 +0900

    추가: taobao_search
    -타오바오에서 검색을 하는 taobao_search 추가
    -taobao_login(driver)은 타오바오 로그인 할 수 있도록 페이지 이동하고 60초간 대기함(사용자 로그인 필요)
    -taobao_image_search(driver, img_path)는 이미지 img_path의 이미지 주소를 기반으로 이미지 검색함

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import time
+
 from flask import Flask
 from routers import routers
 from utills.SingletonWebDriver import SingletonWebDriver
-from utills.save_urls_image_in_directory import save_urls_image_in_directory
+from utills.save_image_and_return_abs_path import save_image_and_return_abs_path
+from utills.get_abs_path import get_abs_path
 from services import market_search
+from services import taobao_search
 from services.pruning_shop_item import pruning_shop_item
 
 app = Flask(__name__)
@@ -18,28 +22,49 @@ def test_sig_func():
     return routers.test_signal()
 
 
+
 @app.route('/market-search')
 def market_search_func():
     driver = SingletonWebDriver.get_driver()
+    taobao_url = 'https://s.taobao.com/search?q='
     min_price = None  # todo: 이것도 사용자에게 받아야함 생략 가능
     max_price = None
 
     keywordlist = routers.key_words()
     print(keywordlist)
 
-    # todo: web띄워서 타오바오 로그인 url 보여주기
-    # 로그인 완료 서버에 알려줘야함...
+    # 타오바오 로그인
+    try:
+        taobao_search.taobao_login(driver)
+    except Exception as e:
+        print("err:", e)
+        print('close webDriver')
+        SingletonWebDriver.close_driver()
+        return e
 
+    # 마켓 검색
     shop_list = market_search.gmarket_search(driver, keywordlist)
     print(shop_list)
+
+    # 아이템 가지 치기
     shop_items = pruning_shop_item(driver, shop_list, min_price, max_price)
     print(f'{shop_items},\r\n'
           f'{len(shop_items)}개')
 
-    image_urls = [item.item_image_url for item in shop_items]
-    save_images_id = save_urls_image_in_directory(image_urls)
+    # 타오바오 내에서 이미지 검색
+    # 필요한 내용 크롤링(이미지, 상품 링크)
+    cnt = 0
+    driver.get(taobao_url)
 
-    # todo: 타오바오 이미지 검색 및 요소 가져와야함
+    for item in shop_items:
+        image_path = save_image_and_return_abs_path(item.item_image_url)
+        if image_path:
+            taobao_item_link, taobao_image_link = taobao_search.taobao_image_search(driver, image_path)
+            print(cnt + 1)
+            item.item_taobao_item_url = taobao_item_link
+            item.item_taobao_image_url = taobao_image_link
+        cnt += 1
+        time.sleep(3)
 
     # todo: 배송비 측정
 
---
+++ services/taobao_search.py
@@ -0,0 +1,74 @@
+import time
+from bs4 import BeautifulSoup
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def taobao_login(driver):
+    url = ('https://login.taobao.com/member/login.jhtml?spm=a21n57.1.754894437.1.491d523cCRi6o6&f=top&redirectURL'
+           '=https%3A%2F%2Fs.taobao.com%2Fsearch%3Fq%3D%26type%3Dp%26tmhkh5%3D%26from%3Dsea_1_searchbutton%26catId'
+           '%3D100%26spm%3Da2141.241046-kr.searchbar.d_2_searchbox')
+    driver.get(url)
+
+    try:
+        WebDriverWait(driver, 60).until(
+            expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="J_SiteNavLogin"]/div[1]'))
+        )
+        print("로그인 성공")
+
+    except Exception as e:
+        Exception("로그인 실패 또는 시간 초과: ", e)
+
+
+def taobao_image_search(driver, img_path):
+    try:
+        upload_input = driver.find_element(By.CSS_SELECTOR, 'input[type="file"]')
+        search_button = driver.find_element(By.ID, 'image-search-upload-button')
+        upload_input.send_keys(img_path)
+        time.sleep(1)
+        search_button.click()
+        print("이미지 검색 완료")
+    except Exception as e:
+        print("err:", e)
+        return 0
+
+    try:
+        load_position = '//*[@id="pageContent"]/div[1]/div[2]/div/div/div[1]/a'
+        WebDriverWait(driver, 15).until(
+            expected_conditions.presence_of_element_located((By.XPATH, load_position))
+        )
+        time.sleep(2)
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        search_item = soup.select_one('a[class^="ImgSearchCard--doubleCardWrapper--"]')
+        print(search_item)
+
+        if search_item:
+            try:
+                item_url = search_item['href']
+            except KeyError:
+                print("타오바오 아이템 URL 서치 실패")
+                item_url = None
+
+            try:
+                image_tag = search_item.select_one('div[class^="MainPic--mainPicWrapper--"] img')
+                image_url = image_tag['src'] if image_tag else None
+            except KeyError:
+                print("타오바오 이미지 서치 실패")
+                image_url = None
+
+            if item_url and image_url:
+                taobao_item_info = [item_url, image_url]
+                return taobao_item_info
+            else:
+                print("필수 정보를 모두 찾지 못했습니다.")
+                return None
+        else:
+            print("검색 아이템을 찾을 수 없습니다.")
+            return None
+
+    except TimeoutException as e:
+        print("페이지 로드 중 오류 발생:", e)
+        return None
