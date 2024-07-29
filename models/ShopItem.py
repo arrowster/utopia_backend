209f93869df1f129584811bd76773646c5257dd9
arrowster <kjy9835@naver.com>
2024-07-29 15:56:38 +0900

    수정: ShopItem 모델 요소 추가
    -taobao에서 수집한 이미지 주소를 담을 item_taobao_image_url 추가

--- models/ShopItem.py
+++ models/ShopItem.py
@@ -8,5 +8,6 @@ class ShopItem:
     item_image_url: str
     item_naver_category: str = field(default_factory=str)
     item_taobao_item_url: str = field(default_factory=str)
+    item_taobao_image_url: str = field(default_factory=str)
     item_main_keywords: List[str] = field(default_factory=list)
     item_sub_keywords: List[str] = field(default_factory=list)
