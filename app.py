34444d2207939f440eed5cfb4f72292942f89c98
arrowster <kjy9835@naver.com>
2024-08-27 17:50:37 +0900

    추가: 수집 시간 측정, 수집 갯수 제한 기능 추가
    -시작 시간, 끝난 시간 측정해 총 걸린 시간 출력
    -수집 최대량까지만 수집하는 기능 추가
    -json 형식으로 반환되도록 수정

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import time
 
-from flask import Flask
+from flask import Flask, jsonify
+from dataclasses import asdict
 from routers import routers
 from utills.SingletonWebDriver import SingletonWebDriver
 from utills.save_image_and_return_abs_path import save_image_and_return_abs_path
+from utills.print_running_time import print_running_time
+from utills.convert_sets_to_lists import convert_sets_to_lists
 from services import market_search
 from services import taobao_search
 from services.pruning_shop_item import pruning_shop_item, search_seb_keywords
@@ -26,6 +29,7 @@ def test_sig_func():
 def market_search_func():
     driver = SingletonWebDriver.get_driver()
     taobao_url = 'https://s.taobao.com/search?q='
+    max_cnt_item = 50 # todo: 이것도 사용자에게 받아야함 생략 불가 단위 50
     min_price = None  # todo: 이것도 사용자에게 받아야함 생략 가능
     max_price = None
 
@@ -40,10 +44,13 @@ def market_search_func():
         print('close webDriver')
         SingletonWebDriver.close_driver()
         return e
+    start_time = time.localtime()
+    print(f'수집 시작 시간: {start_time.tm_hour}:{start_time.tm_min}:{start_time.tm_sec}')
 
     # 마켓 검색
     shop_list = market_search.gmarket_search(driver, keywordlist)
     print(shop_list)
+    print(f'{len(shop_list)}개의 마켓을 찾음')
 
     # 아이템 가지 치기 (아이템 이름, 이미지, 네이버 카테고리, 메인 키워드 수집)
     shop_items = pruning_shop_item(driver, shop_list, min_price, max_price)
@@ -52,7 +59,6 @@ def market_search_func():
 
     # 서브 키워드 수집
     main_keyword_to_sub_keywords = {}
-
     for item in shop_items:
         main_keyword = item.item_main_keywords
         if main_keyword not in main_keyword_to_sub_keywords:
@@ -67,6 +73,11 @@ def market_search_func():
     cnt = 0
     driver.get(taobao_url)
 
+    if max_cnt_item > len(shop_items): #
+        all_item_cnt = len(shop_items)
+    else:
+        all_item_cnt = max_cnt_item
+
     # 타오바오 링크, 타오바오 이미지 수집
     for item in shop_items:
         image_path = save_image_and_return_abs_path(item.item_image_url)
@@ -77,17 +88,28 @@ def market_search_func():
             else:
                 continue
 
-            print(f'{cnt + 1} / {len(shop_items)}')
+            print(f'{cnt + 1} / {all_item_cnt}')
             item.item_taobao_item_url = taobao_item_link
             item.item_taobao_image_url = taobao_image_link
         cnt += 1
+        if cnt >= max_cnt_item:
+            break
         time.sleep(2)
-    print(f'누락 {len(shop_items) - cnt}개')
+
+    print(f'누락 {all_item_cnt - cnt}개') # 최대수 따로 뽑아서 해야함
     print(shop_items)
 
-    print('close webDriver')
+    end_time = time.localtime()
+    print(f'수집 종료 시간: {end_time.tm_hour}:{end_time.tm_min}:{end_time.tm_sec}')
     SingletonWebDriver.close_driver()
-    return keywordlist
+
+    print_running_time(start_time, end_time)
+    print(f'수집 아이템 수: {cnt}개')
+
+    shop_items_dict = [asdict(item) for item in shop_items]
+    shop_items_list = convert_sets_to_lists(shop_items_dict)
+    print('done.')
+    return jsonify(shop_items_list)
 
 
 if __name__ == '__main__':
